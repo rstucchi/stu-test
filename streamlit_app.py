1e5d5b641aba8722b87f687f0258f1a38ccb8402
rstucchi <47424057+rstucchi@users.noreply.github.com>
2024-10-31 07:12:08 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+import lxml
 
 # Show the page title and description.
 st.set_page_config(page_title="Orario bus", page_icon="🚌")
@@ -12,5 +13,5 @@ st.write(
 )
 
 # Read table
-#url = r'https://www.asfautolinee.it/subpage_orari_osm.php?lang=it&linea=N_1&direzione=1&fermata=COMO_A07'
-#tables = pd.read_html(url)
+url = r'https://www.asfautolinee.it/subpage_orari_osm.php?lang=it&linea=N_1&direzione=1&fermata=COMO_A07'
+tables = pd.read_html(url)
